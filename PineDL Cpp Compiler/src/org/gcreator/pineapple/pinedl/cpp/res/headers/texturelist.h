d417d88fc19d83e8d0942ca53c0be8337bf7638b
bobserge <bobtheblueberry@gmail.com>
2009-06-10 01:45:45 +0000

    git-svn-id: https://svn.code.sf.net/p/pineapple-ide/svn/Sabre/Trunk@515 1b6dd1f4-0bef-4bc2-93b5-a664bc25159d

---
+++ PineDL Cpp Compiler/src/org/gcreator/pineapple/pinedl/cpp/res/headers/texturelist.h
@@ -0,0 +1,37 @@
+#ifndef __PINEDL_TEXTURELIST_H__
+#define __PINEDL_TEXTURELIST_H__
+
+#include "texture.h"
+#include <vector>
+
+using namespace Pineapple;
+using namespace std;
+
+namespace Pineapple
+{
+    class TextureList
+    {
+    protected:
+        static Texture* Load_Compressed_Texture(FILE*, unsigned int, unsigned int);
+        static Texture* Load_Texture(char*, unsigned int);
+        static void Load_Textures(unsigned int* ids, unsigned int n);
+        static void load();
+        struct ImageData
+        {
+            unsigned long long size;
+            unsigned long long decompressed_size;
+            Texture* tex;
+        };
+        static vector<ImageData*> images;
+        static long archive_size;
+        static long archive_data_start;
+        static unsigned int* START_IMAGES;
+        static unsigned int N_START_IMAGES;
+    public:
+        static void init();
+        static Texture* Get_Texture(unsigned int);
+    };
+}
+
+
+#endif
