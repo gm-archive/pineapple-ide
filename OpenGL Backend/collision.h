42b39d8e6c0a8e90f8a7abebd66cde103b25aa3f
luiscubal <luiscubal@gmail.com>
2009-06-27 14:36:19 +0000

    Fixed collision.h in OpenGL Backend. Added RectangleIsTangent implementation.
    git-svn-id: https://svn.code.sf.net/p/pineapple-ide/svn/Sabre/Trunk@568 1b6dd1f4-0bef-4bc2-93b5-a664bc25159d

--- OpenGL Backend/collision.h
+++ OpenGL Backend/collision.h
@@ -4,7 +4,7 @@
 class CollisionManager
 {
     public:
-        static bool RectangleIntersects(Rectangle* r1, Rectangle* r2)
+        static bool rectangleIntersects(Rectangle* r1, Rectangle* r2)
         {
             Point* r1p = r1->getTopLeftPoint();
             double r1x = r1p->getX();
@@ -16,10 +16,29 @@ class CollisionManager
             double r2y = r2p->getY();
             double r2width = r2->getWidth();
             double r2height = r2->getHeight();
-            if(r1x>r2x+r2width) return false;
-            if(r1x+r1width<r2x) return false;
-            if(r1y>r2y+r2height) return false;
-            if(r1y+r1height<r2y) return false;
+            if(r1x>=r2x+r2width) return false;
+            if(r1x+r1width<=r2x) return false;
+            if(r1y>=r2y+r2height) return false;
+            if(r1y+r1height<=r2y) return false;
+            return true;
+        }
+
+	static bool rectangleIsTangent(Rectangle* r1, Rectangle* r2)
+        {
+            Point* r1p = r1->getTopLeftPoint();
+            double r1x = r1p->getX();
+            double r1y = r1p->getY();
+            double r1width = r1->getWidth();
+            double r1height = r1->getHeight();
+            Point* r2p = r2->getTopLeftPoint();
+            double r2x = r2p->getX();
+            double r2y = r2p->getY();
+            double r2width = r2->getWidth();
+            double r2height = r2->getHeight();
+            if(r1x==r2x+r2width) return false;
+            if(r1x+r1width==r2x) return false;
+            if(r1y==r2y+r2height) return false;
+            if(r1y+r1height==r2y) return false;
             return true;
         }
 };
