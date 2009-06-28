926353b9e4fcbbe451028e57b330bc4ae714859d
luiscubal <luiscubal@gmail.com>
2009-06-28 15:28:44 +0000

    Fixed a problem with templates.
    git-svn-id: https://svn.code.sf.net/p/pineapple-ide/svn/Sabre/Trunk@581 1b6dd1f4-0bef-4bc2-93b5-a664bc25159d

--- OpenGL Backend/core.h
+++ OpenGL Backend/core.h
@@ -24,21 +24,6 @@ namespace Pineapple{
     template<class T> bool _isInstanceof(bool o){ return false; }
     template<class T> bool _isInstanceof(std::string o){ return false; }
 
-    template<> bool _isInstanceof<int>(Object* o){ return false; }
-    template<> bool _isInstanceof<int>(int o){ return true; }
-
-    template<> bool _isInstanceof<double>(Object* o){ return false; }
-    template<> bool _isInstanceof<double>(double o){ return true; }
-
-    template<> bool _isInstanceof<char>(Object* o){ return false; }
-    template<> bool _isInstanceof<char>(char o){ return true; }
-
-    template<> bool _isInstanceof<bool>(Object* o){ return false; }
-    template<> bool _isInstanceof<bool>(bool o){ return true; }
-
-    template<> bool _isInstanceof<std::string>(Object* o){ return false; }
-    template<> bool _isInstanceof<std::string>(std::string o){ return true; }
-
 }
 
 #endif
