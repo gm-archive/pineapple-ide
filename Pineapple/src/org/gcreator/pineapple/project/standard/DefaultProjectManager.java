1b64b227cf41d215c979e79b873ad409f83cefd2
luiscubal <luiscubal@gmail.com>
2009-06-28 15:51:07 +0000

    Fixed bug in copy implementation.
    git-svn-id: https://svn.code.sf.net/p/pineapple-ide/svn/Sabre/Trunk@584 1b6dd1f4-0bef-4bc2-93b5-a664bc25159d

--- Pineapple/src/org/gcreator/pineapple/project/standard/DefaultProjectManager.java
+++ Pineapple/src/org/gcreator/pineapple/project/standard/DefaultProjectManager.java
@@ -412,7 +412,7 @@ public class DefaultProjectManager implements ProjectManager {
                 out = new BufferedOutputStream(new FileOutputStream(newFile));
                 int read;
                 byte[] buffer = new byte[8192];
-                while ((read = in.read()) != -1) {
+                while ((read = in.read(buffer)) != -1) {
                     out.write(buffer, 0, read);
                 }
                 updateTreeUI();
