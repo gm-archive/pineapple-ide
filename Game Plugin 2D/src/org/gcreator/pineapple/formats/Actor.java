452be13315831fb34b55e3b54ee8a1f4ee934a3d
bobserge <bobtheblueberry@gmail.com>
2009-09-03 01:07:07 +0000

    ...
    git-svn-id: https://svn.code.sf.net/p/pineapple-ide/svn/Sabre/Trunk@638 1b6dd1f4-0bef-4bc2-93b5-a664bc25159d

--- Game Plugin 2D/src/org/gcreator/pineapple/formats/Actor.java
+++ Game Plugin 2D/src/org/gcreator/pineapple/formats/Actor.java
@@ -1,6 +1,6 @@
 /*
 Copyright (C) 2008 Luís Reis<luiscubal@gmail.com>
-Copyright (C) 2008, 2009 Serge Humphrey<bob@bobtheblueberry.com>
+Copyright (C) 2008, 2009 Serge Humphrey<serge@bobtheblueberry.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -99,7 +99,9 @@ public class Actor extends ClassResource {
         if (i > 0) {
             this.setName(file.getName().substring(0, i));
         }
-        new ActorImporter(this, new BufferedInputStream(file.getReader()));
+        if (file.getLength() != 0) {
+            new ActorImporter(this, new BufferedInputStream(file.getReader()));
+        }
     }
 
     /**
